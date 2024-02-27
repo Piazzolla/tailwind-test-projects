c39ada37c08854f0ebec67a39ceaedfbaa58e397
Mariano <macacozzz@hotmail.com>
2024-02-27 17:20:47 -0300

    creations section container

--- projects/loopstudios/tailwind.config.js
+++ projects/loopstudios/tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
         alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.5em',
       }
     },
   },
