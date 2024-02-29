a58359f96ecffbb7206e5392b73c13ae3f16e5b5
Mariano <macacozzz@hotmail.com>
2024-02-29 10:24:51 -0300

    fix loopstudios comments - added hamburger button and mobile menu to shortly

--- projects/shortly/js/script.js
+++ projects/shortly/js/script.js
@@ -33,3 +33,14 @@ function formSubmit(e) {
     }
 
 }
+
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
+btn.addEventListener('click', navToggle);
+
+function navToggle() {
+    btn.classList.toggle('open');
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
+}
