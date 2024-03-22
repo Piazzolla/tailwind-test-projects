e27c18f75de0862d8747677b633633f2b60de5bb
Mariano <macacozzz@hotmail.com>
2024-03-21 22:42:30 -0300

    project setup

--- projects/bookmark/tailwind.config.js
+++ projects/bookmark/tailwind.config.js
@@ -2,8 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+      },
+    },
+    plugins: [],
+  }
 }
-
